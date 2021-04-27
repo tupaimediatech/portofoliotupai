399cde600eeaf9425fa7283b3d6fcaf76a43a478
giovannydani <giovannysaputra14@gmail.com>
2021-04-27 14:13:32 +0700

    detail-services

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,53 @@ $(window).on('load', function() {
 });
 
 $(document).ready(function(){
+
   $('.scrollToTop').click(function(){
     $('html, body').animate({scrollTop : 0},800);
     return false;
   });
+
+  $('a.scroll-to').on('click',function(e) {
+    if (this.hash !=="") {
+      
+      e.preventDefault();
+  
+      var hash = this.hash;
+      var height_navbar = $('nav.navbar-scrolled').outerHeight( true );
+      var koordinat=$(hash).offset().top;
+      var koordinat2=$(hash).offset().top-=62;
+      console.log(koordinat);
+      console.log(koordinat-=2);
+
+
+      $('html, body').animate({
+        scrollTop: koordinat2
+      }, 800, function() {
+        window.location.hash = hash;
+      })
+  
+    }
+  });
+
+});
+
+$(document).ready(function () {
+  
+  var Counter= 1;
+  var navlength = $('a.nav-services-home').length;
+
+  setInterval(function(){
+    
+    
+    // $('a.nav-services-home').next().click;
+    $('a.ns-'+Counter).click();
+    
+    Counter+=1;
+
+    if (Counter > navlength) {
+      Counter=1;
+    }
+  }, 5000);
 });
 
 // slick slider
